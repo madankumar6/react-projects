786263607a8061a315e7d4d0b2aac433b548661e
madankumar6 <madankumar6@gmail.com>
2024-10-31 23:49:19 +0530

    Updated code for adding a new project

--- project-management/src/App.jsx
+++ project-management/src/App.jsx
@@ -1,12 +1,54 @@
 import ProjectsSidebar from "./components/ProjectsSideBar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+        return {
+          ...prevState,
+          selectedProjectId: null
+        };
+      }
+    );
+  }
+
+  function handleAddProject(projectdData) {
+    const newProject = {
+      ...projectdData,
+      id: Math.random()
+    };
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      };
+    }
+  );
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject}></NewProject>;
+  }
+  else if(projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}></NoProjectSelected>;
+  }
+
+  console.log(projectsState);
+
   return (
     <>
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar></ProjectsSidebar>
-      <NewProject></NewProject>
+      <ProjectsSidebar onStartAddProject={handleStartAddProject}></ProjectsSidebar>
+      {content}      
     </main>
     </>
   );
