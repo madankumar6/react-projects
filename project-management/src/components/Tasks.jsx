acec70fa8c95a460553a50aebd46c39ce1f6e390
madankumar6 <madankumar6@gmail.com>
2024-11-04 18:50:36 +0530

    Started adding tasks for the PMP

---
+++ project-management/src/components/Tasks.jsx
@@ -0,0 +1,8 @@
+export default function Tasks() {
+    return (
+        <section>
+            <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+            <p className="text-stone-800 mb-4 my-4">This project doesn't have any tasks yet!</p>
+        </section>
+    );
+}
